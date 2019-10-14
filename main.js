eb7d110bdb69fef676e0070fe140d5cf4f398af2
Thatguyjs <46634731+Thatguyjs@users.noreply.github.com>
2019-10-14 13:04:22 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const Compiler = require("./compiler.js");
+const FS = require("fs");
+
+let data = Compiler.eval(FS.readFileSync('./code.rawm', {encoding: 'utf-8'}));
+
+FS.writeFileSync('./result.mexe', data);
