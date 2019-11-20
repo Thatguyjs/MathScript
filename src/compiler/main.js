b69fa182940c105d6e6782ad6c9ca72c14c0a9d9
Thatguyjs <46634731+Thatguyjs@users.noreply.github.com>
2019-11-19 19:47:59 -0700

    Delete main.js

--- src/compiler/main.js
+++
@@ -1,6 +0,0 @@
-const Compiler = require("./compiler.js");
-const FS = require("fs");
-
-let data = Compiler.eval(FS.readFileSync('../code.rawm', {encoding: 'utf-8'}));
-
-FS.writeFileSync('../result.mexe', data);
