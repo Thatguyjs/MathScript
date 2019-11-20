1d68623485689af0a530cfb8f212b1e56cff7e5d
Thatguyjs <46634731+Thatguyjs@users.noreply.github.com>
2019-11-19 19:50:04 -0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,29 @@
+const process = require("process");
+
+const Compiler = require("./compiler/main.js");
+// const Parser = require("./parser/main.js");
+
+
+// Construct a command to compile or parse
+function construct(args) {
+	let command = {
+		filepath: args[0]
+	};
+
+	if(command.filepath[0] !== '.' && !command.filepath[1] !== ':') {
+		command.filepath = './' + command.filepath;
+	}
+
+	return command;
+}
+
+
+function compile(command) {
+	Compiler.load(command.filepath, (code) => {
+		if(code !== 0) console.log("Error");
+		else Compiler.run();
+	});
+}
+
+
+compile(construct(process.argv.slice(2)));
